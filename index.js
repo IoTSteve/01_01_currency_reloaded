964c0ca4ff1244e1598cc95e3eff7507897fe575
Steve <S_Reich.1@web.de>
2019-11-26 11:33:34 +0100

    working version release

--- index.js
+++ index.js
@@ -37,3 +37,48 @@ if (args.length < 3) {
   originalCurrency = args[1];
   targetCurrency = args[2];
 }
+
+//variable output
+let output
+//währungswerte auf dollar normiert
+const currencies = {
+  USD: {conversion: 1, sign: '$'},
+  EUR: {conversion: 0.9, sign: '€'},
+  YEN: {conversion: 108.77, sign: '@'}
+
+}
+
+
+const request = require('request');
+request('https://api.exchangeratesapi.io/latest', function (error, response, body) {
+  console.error('error:', error); // Print the error if one occurred
+  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  //console.log('body:', body); // Print the HTML for the Google homepage.
+  let bodyObj = JSON.parse(body);
+
+
+  try {
+    for (const pos in bodyObj.rates) {
+      if (currencies.hasOwnProperty(pos)) {
+        //const celement = download.rates[pos]
+        currencies[pos].rate = bodyObj.rates[pos];
+      } else if (currencies.hasOwnProperty(pos) === false) {
+        currencies[pos] = {};
+        currencies[pos].rate = bodyObj.rates[pos];
+      }
+    }
+  // eslint-disable-next-line no-empty
+  } catch (error) {
+    
+  }
+
+  const ammountInUSD = amount / currencies[originalCurrency].conversion;
+
+  output = ammountInUSD * currencies[targetCurrency].conversion;
+
+  console.log(output)
+
+
+});
+
+
